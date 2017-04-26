b982201feb76f2eec5dbca679e4f7c00ad0a0d71
hang.su <tomhon@qq.com>
2017-04-26 18:27:48 +0800

    add mysql data sql

---
+++ src/main/resources/sql/mysql_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO `movie` (`name`, `description`)
+VALUES ('A Chinese Odyssey Part One: Pandora\'s Box', '大话西游之月光宝盒');
+INSERT INTO `movie` (`name`, `description`)
+VALUES ('A Chinese Odyssey Part Two: Cinderella', '大话西游之大圣娶亲');
+INSERT INTO `movie` (`name`, `description`)
+VALUES ('A Chain of Short Stories about Their Distance', '秒速五厘米');
